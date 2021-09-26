a1f6b9b81537cc5b7d7b77f1ebe47b4050c9be86
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-26 17:32:29 +0300

    creates data access classess

---
+++ src/main/java/dao/Sql2oPostsDao.java
@@ -0,0 +1,64 @@
+package dao;
+
+import models.Post;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oPostsDao implements PostsDao {
+    private final Sql2o sql2o;
+    public Sql2oPostsDao (Sql2o sql2o){
+        this.sql2o= sql2o;
+    }
+
+    @Override
+    public void add(Post post) {
+        String sql = "INSERT INTO posts (author, title, content, id) VALUES (:author, :title, :content, :id)";
+        try (Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(post)
+                    .executeUpdate()
+                    .getKey();
+            post.setId(id);
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        try (Connection con = sql2o.open()){
+            return con.createQuery("SELECT * FROM posts")
+                    .executeAndFetch(Post.class);
+        }
+    }
+
+//    @Override
+//    public Post findById(int id) {
+//        return null;
+//    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE from posts WHERE id = :id";
+        try (Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public void clearAll() {
+        String sql = "DELETE from posts";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql).executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
+}
