b518ae334b8709c111da0d6b90b3e30f02cec0fc
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-27 07:15:26 +0300

    Adds routes to the main class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,48 @@
 
 import com.google.gson.Gson;
+import dao.Sql2oPostsDao;
+import models.Departments;
+import models.Post;
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oPostsDao;
+import org.sql2o.Sql2o;
+
+import java.sql.Connection;
+
+import static spark.Spark.*;
+
 
 public class App {
     public static void main(String[] args) {
 
-
-        sql2oDepartments departments;
-        sql2oNews sql2oNew ;
-        Connection conn;
+        Sql2oDepartmentsDao departments;
+        Sql2oPostsDao sql2opost;
+        Connection con;
         Gson gson = new Gson();
 
         String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
 
-        departments = new sql2oDepartments(sql2o);
-        sql2oNew = new sql2oNews(sql2o);
-//        conn = sql2o.open();
+        departments = new Sql2oDepartmentsDao(sql2o);
+        sql2opost = new Sql2oPostsDao(sql2o);
 
         post("/department/new", "application/json", (req, res) -> {
-            Departments department1 = gson.fromJson(req.body(), Departments.class);
-            departments.create(department1);
+            Departments department = gson.fromJson(req.body(), Departments.class);
+            departments.create(department);
             res.status(201);
             res.type("application/json");
-            return gson.toJson(department1);
+            return gson.toJson(department);
         });
 
         post("/news/new", "application/json", (req, res) -> {
-            News news = gson.fromJson(req.body(), News.class);
-            sql2oNew.create(news);
+            Post post = gson.fromJson(req.body(), Post.class);
+            sql2opost.create(post);
             res.status(201);
             res.type("application/json");
-            return gson.toJson(news);
+            return gson.toJson(post);
         });
 
     }
 }
+
+
