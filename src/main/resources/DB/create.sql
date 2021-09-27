e7bce24868880c7e407df9e9c7d77b405f71a45d
KyleTrippK <kyle.kibet@student.moringaschool.com>
2021-09-27 07:14:09 +0300

    Creates the scripts to manipulae the databse

--- src/main/resources/DB/create.sql
+++ src/main/resources/DB/create.sql
@@ -1,25 +1,33 @@
-SET MODE PostgreSQL;
-
+--SET MODE PostgreSQL;
+--
 CREATE DATABASE news;
+CREATE DATABASE news_test WITH TEMPLATE news;
 \c news
+
 CREATE TABLE IF NOT EXISTS users (
- id int PRIMARY KEY auto_increment,
+ id SERIAL PRIMARY KEY,
  name VARCHAR,
  userPosition VARCHAR,
  userDeparment VARCHAR,
- role VARCHAR,
+ role VARCHAR
 );
 
 CREATE TABLE IF NOT EXISTS departments (
- id int PRIMARY KEY auto_increment,
+ id SERIAL PRIMARY KEY,
  title VARCHAR,
  description VARCHAR,
  members INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS posts (
- id int PRIMARY KEY auto_increment,
+ id SERIAL PRIMARY KEY,
  author VARCHAR,
  title VARCHAR,
- content VARCHAR,
+ content VARCHAR
 );
+
+CREATE TABLE IF NOT EXISTS user_departments(
+id SERIAL PRIMARY KEY,
+user_id INTEGER,
+department_id INTEGER
+)
